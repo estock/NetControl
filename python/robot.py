dba5bd3f32f561b7895a2d1661bd9cd8874cede6
yourname <your@email.com>
2013-10-18 17:33:22 -0400

    sa

--- python/robot.py
+++ python/robot.py
@@ -8,18 +8,13 @@ from socketIO_client import SocketIO
 def main():
 
     try:
-        # initialize serial port
-        serialPort = serial.Serial(port='/dev/ttyUSB0',baudrate='9600')
-        serialPort.flushInput()
-
-        # initialize websocket client
-        piClient = PiClient('ws://ec2-23-20-219-99.compute-1.amazonaws.com:8080/ws', serialPort)
-        piClient.connect()
 
         while True:
-        # keep websocket alive by sending ping on interval
+            # keep websocket alive by sending ping on interval
+            piClient.ping()
 
-            with SocketIO('ws://node.labsmb.com', 8990) as socketIO:
+            # look for incoming serial data
+            with SocketIO('node.labsmb.com', 8990) as socketIO:
                 socketIO.emit('bbb', {'xxx': 'yyy'}, on_bbb_response)
                 socketIO.wait_for_callbacks(seconds=1)
 
@@ -27,8 +22,11 @@ def main():
         piClient.close()
         serialPort.close()
 
+
+
 def on_bbb_response(*args):
     print 'on_bbb_response', args
+
 
 if __name__ == "__main__":
     main()
