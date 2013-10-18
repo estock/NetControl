52df881ee02769927b3192e67bfcb95c86d1cd0c
yourname <your@email.com>
2013-10-18 16:57:47 -0400

    first commit
    first commit

---
+++ python/robot.py
@@ -0,0 +1,10 @@
+__author__ = 'eric.stockmeyer'
+
+from socketIO_client import SocketIO
+
+def on_bbb_response(*args):
+    print 'on_bbb_response', args
+
+with SocketIO('ws://node.labsmb.com', 8990) as socketIO:
+    socketIO.emit('bbb', {'xxx': 'yyy'}, on_bbb_response)
+    socketIO.wait_for_callbacks(seconds=1)
